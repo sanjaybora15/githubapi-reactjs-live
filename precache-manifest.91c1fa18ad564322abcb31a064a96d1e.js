2e5dbb31b775086a050c5fb33abce9f93eff9a04
sanjaybora15 <sanjaybora15@gmail.com>
2019-09-24 23:06:37 +0530

    Updates

---
+++ precache-manifest.91c1fa18ad564322abcb31a064a96d1e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "779a0bb84786ab786edc3f8f8c3971be",
+    "url": "/githubapi-reactjs-live/index.html"
+  },
+  {
+    "revision": "e6806ff662d921891675",
+    "url": "/githubapi-reactjs-live/static/css/main.806f8953.chunk.css"
+  },
+  {
+    "revision": "2e40eb80d4772657eaa2",
+    "url": "/githubapi-reactjs-live/static/js/2.852e88c8.chunk.js"
+  },
+  {
+    "revision": "e6806ff662d921891675",
+    "url": "/githubapi-reactjs-live/static/js/main.430dab58.chunk.js"
+  },
+  {
+    "revision": "98f53e2199a70b94316c",
+    "url": "/githubapi-reactjs-live/static/js/runtime-main.55cb4b49.js"
+  }
+]);
